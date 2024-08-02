71945f2325fdbce3b7eed3203b409db15271b858
NTX STEALTH <rohitrajan2005@gmail.com>
2024-08-02 22:54:59 +0530

    signifigant changes to the structure and fixed the product cards.

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const product_button = document.getElementById('Products')
+const product_section =document.getElementById("Product-section")
+
+product_button.addEventListener('click', () => { product_section.scrollTo(); console.log(50);});
